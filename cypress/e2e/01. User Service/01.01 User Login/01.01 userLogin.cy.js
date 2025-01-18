ce84c8b75e64ebd9eefa04d74588772ed86d869a
Anonno Das <anonnodas97@gmail.com>
2025-01-18 21:18:53 +0600

    create new project for branch

---
+++ cypress/e2e/01. User Service/01.01 User Login/01.01 userLogin.cy.js
@@ -0,0 +1,44 @@
+describe("User login into branch with valid credential and get status code 200", () => {
+  let userEmail;
+  let userPassword;
+  before(() => {
+    cy.readFile("cypress/fixtures/userInformation.json").then((data) => {
+      userEmail = data.email;
+      userPassword = data.password;
+    });
+  });
+
+  it("Checking if should be able to login user into branch", () => {
+    cy.request({
+      method: "POST",
+      url: "/admin/branch/login",
+      body: {
+        slug: "first-branch-pgbr",
+        email: userEmail,
+        password: userPassword,
+        branch: "first-branch-pgbr",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      if (response.status === 200) {
+        const { token } = response.body;
+
+        cy.writeFile("cypress/fixtures/userToken.json", {
+          userAccessToken: token,
+        });
+
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property("token");
+        expect(response.duration).to.be.lessThan(2000);
+        cy.log("response.body", JSON.stringify(response.body, null, 1));
+        console.log("response.body", JSON.stringify(response.body, null, 1));
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
