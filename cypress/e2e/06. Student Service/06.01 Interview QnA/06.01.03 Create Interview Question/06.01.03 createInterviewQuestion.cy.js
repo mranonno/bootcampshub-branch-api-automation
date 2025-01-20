b2fa1ffe308dfc65b7b46a450161da4afc645dae
Anonno Das <anonnodas97@gmail.com>
2025-01-20 20:26:56 +0600

    Student Service interview question add successfully

---
+++ cypress/e2e/06. Student Service/06.01 Interview QnA/06.01.03 Create Interview Question/06.01.03 createInterviewQuestion.cy.js
@@ -0,0 +1,50 @@
+describe.skip("Create interview question with status code 200", () => {
+  let accessToken;
+  let branchId;
+  before(() => {
+    cy.readFile("cypress/fixtures/userToken.json").then((data) => {
+      accessToken = data.userAccessToken;
+    });
+    cy.readFile("cypress/fixtures/branchId.json").then((branch) => {
+      branchId = branch.branchId;
+    });
+  });
+  it("Checking if should be able to create interview question", () => {
+    cy.request({
+      method: "POST",
+      url: "/interviewqa/add",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Branch: branchId,
+      },
+      body: {
+        name: "How can we API automation by cypress?",
+        programs: ["6501f8b21c01cf0019303d0b"],
+        sessions: ["664a63c81a88ea0019515a20"],
+        isActive: "active",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
---
+++ cypress/e2e/06. Student Service/06.01 Interview QnA/06.01.04 Edit Interview Question/06.01.04 editInterviewQuestion.cy.js
@@ -0,0 +1,50 @@
+describe.skip("Edit interview question with status code 200", () => {
+  let accessToken;
+  let branchId;
+  before(() => {
+    cy.readFile("cypress/fixtures/userToken.json").then((data) => {
+      accessToken = data.userAccessToken;
+    });
+    cy.readFile("cypress/fixtures/branchId.json").then((branch) => {
+      branchId = branch.branchId;
+    });
+  });
+  it("Checking if should be able to edit interview question", () => {
+    cy.request({
+      method: "PATCH",
+      url: "/interviewqa/edit/66dcbd03a828723fac204574",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Branch: branchId,
+      },
+      body: {
+        name: "test update",
+        programs: [],
+        sessions: ["6501f8b21c01cf0019303d0b", "64fcb957b0cf6e9ae43d126d"],
+        isActive: "inactive",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
