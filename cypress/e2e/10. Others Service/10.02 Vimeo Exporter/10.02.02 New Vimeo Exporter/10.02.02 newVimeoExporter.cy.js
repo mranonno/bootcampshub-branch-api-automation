6596679cfd23aee8a67e5c5dbdfd23995b3ffe2e
Anonno Das <anonnodas97@gmail.com>
2025-01-26 17:52:04 +0600

    vimeo and trail member added successfully

---
+++ cypress/e2e/10. Others Service/10.02 Vimeo Exporter/10.02.02 New Vimeo Exporter/10.02.02 newVimeoExporter.cy.js
@@ -0,0 +1,47 @@
+describe("New vimeo exporter with status code 200", () => {
+  let accessToken;
+  let branchId;
+  before(() => {
+    cy.readFile("cypress/fixtures/userToken.json").then((data) => {
+      accessToken = data.userAccessToken;
+    });
+    cy.readFile("cypress/fixtures/branchId.json").then((branch) => {
+      branchId = branch.branchId;
+    });
+  });
+  it("Checking if should be able to new vimeo exporter", () => {
+    cy.request({
+      method: "GET",
+      url: "/settings/vimeo?folderId=22453497&page=1&per_page=95&direction=asc&sort=default",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Branch: branchId,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        cy.writeFile("cypress/fixtures/historyId.json", {
+          historyId: body.history._id,
+        });
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
---
+++ cypress/e2e/10. Others Service/10.02 Vimeo Exporter/10.02.03 Delete Vimeo/10.02.03 deleteVimeo.cy.js
@@ -0,0 +1,47 @@
+describe("Delete vimeo history with status code 200", () => {
+  let accessToken;
+  let branchId, historyId;
+  before(() => {
+    cy.readFile("cypress/fixtures/userToken.json").then((data) => {
+      accessToken = data.userAccessToken;
+    });
+    cy.readFile("cypress/fixtures/branchId.json").then((branch) => {
+      branchId = branch.branchId;
+    });
+    cy.readFile("cypress/fixtures/historyId.json").then((history) => {
+      historyId = history.historyId;
+    });
+  });
+  it("Checking if should be able to delete vimeo history", () => {
+    cy.request({
+      method: "DELETE",
+      url: `/settings/vimeo/history/delete/${historyId}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Branch: branchId,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(200, "Expected status code is 200");
+      if (response.status === 200) {
+        const { body, duration } = response;
+        expect(body.success).to.eq(true);
+        expect(duration).to.be.lessThan(
+          2000,
+          "Response time should be less than 2 seconds"
+        );
+        cy.log("Response body (stringified):", JSON.stringify(body, null, 2));
+        console.log(
+          "Response body (stringified):",
+          JSON.stringify(body, null, 2)
+        );
+      } else {
+        cy.log("Request failed with status code:", response.status);
+        cy.log(
+          "Error details:",
+          response.body.error || "No error details provided"
+        );
+      }
+    });
+  });
+});
